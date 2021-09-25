fd848e7a92327d5c0c9a5bb72b50610c39b182d0
ZioEren <gabryprog@gmail.com>
2021-09-25 18:01:47 +0200

    Code optimizations

--- Lexer/Lexer.cs
+++ Lexer/Lexer.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Titanium.Lexer
+public class Lexer
 {
-    class Lexer
-    {
-    }
+
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
-namespace Titanium
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Titanium!");
-        }
+
     }
 }
