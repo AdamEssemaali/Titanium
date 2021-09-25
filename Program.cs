561fc386fb2c740f8316869d7ed67e742bc60e93
ZioEren <gabryprog@gmail.com>
2021-09-25 17:50:35 +0200

    Modified string

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Titanium
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Titanium!");
         }
     }
 }
